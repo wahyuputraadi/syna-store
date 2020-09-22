09cb054e0b7caaf04c056b64e526572a2d44b09c
wahyuputraadi <wahyuadii516@gmail.com>
2020-09-22 17:00:27 +0700

    commit pertama

---
+++ src/components/HeaderSyna.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="header">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-lg-2 m-1">
+          <img class="logo-syna" src="../assets/logo2.png" alt />
+        </div>
+        <div class="col-lg-6 m-1">
+            <input class="form-control" type="text" placeholder="Belanja apa hari ini ... ">
+        </div>
+        <div class="col-lg-1">
+             <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+        </div>
+        <div class="col-lg-2">
+            | <button class="btn btn-outline-warning m-1">Daftar</button>
+              <button class="btn btn-success">Masuk</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+export default {
+  name: "HeaderSyna",
+};
+</script>
+
+<style>
+template{
+    margin: 0px;
+    padding: 0px;
+}
+
+#header .logo-syna {
+  width: 200px;
+  height: auto;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <header-syna></header-syna>
+  </div>
+</template>
+
+<script>
+
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderSyna from "@/components/HeaderSyna.vue";
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderSyna,
+  }
+}
+</script>
